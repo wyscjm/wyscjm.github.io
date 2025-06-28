501e930aa4c2eb28fdf79224862559e03c0f0617
wyscjm <w079064@163.com>
2025-06-28 13:32:55 +0800

    deploy blog at ${NOW}

---
+++ javascripts/site_analytics/baidu_tongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?8d9d9e38d55b65b16a2949ff63c81235";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
